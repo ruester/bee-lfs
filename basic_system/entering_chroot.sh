09bce23d49273333f8ca01218692014188b08eb9
Matthias Ruester <ruester@molgen.mpg.de>
2011-12-30 20:36:31 +0000

    basic system: add LOGNAME variable to chroot environment

--- basic_system/entering_chroot.sh
+++ basic_system/entering_chroot.sh
@@ -7,4 +7,5 @@ cp -vfr $(pwd) $LFS/root
 chroot "$LFS" /tools/bin/env -i \
     HOME=/root TERM="$TERM" PS1="\u:\w$ " \
     PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
+    LOGNAME=root \
     /tools/bin/bash --login +h
