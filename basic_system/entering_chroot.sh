ccc547c816ba28bc4ac1d2c974c523e36aec877b
Matthias Ruester <ruester@molgen.mpg.de>
2011-12-30 20:59:28 +0000

    basic system: fix bee's SRCURL and its copying from the host

--- basic_system/entering_chroot.sh
+++ basic_system/entering_chroot.sh
@@ -5,6 +5,9 @@ LFS=/mnt/lfs
 mkdir -pv $LFS/root
 cp -vfr $(pwd) $LFS/root
 
+mkdir -pv $LFS/usr/src
+cp -vf /usr/src/bee.tar $LFS/usr/src
+
 chroot "$LFS" /tools/bin/env -i \
     HOME=/root TERM="$TERM" PS1="\u:\w$ " \
     PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
