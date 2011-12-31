0def96654d50e0c95afc8dad244545df009ae40f
Matthias Ruester <ruester@molgen.mpg.de>
2011-12-31 00:06:06 +0000

    basic system: set LOGNAME when reentering the chroot environment

--- basic_system/reenter_chroot.sh
+++ basic_system/reenter_chroot.sh
@@ -7,4 +7,5 @@ cp -vfr $(pwd)/../making_bootable $LFS/root
 chroot "$LFS" /usr/bin/env -i \
     HOME=/root TERM="$TERM" PS1='\u:\w\$ ' \
     PATH=/bin:/usr/bin:/sbin:/usr/sbin \
+    LOGNAME=root \
     /bin/bash --login
