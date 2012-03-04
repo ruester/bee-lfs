d488757db428c910be64f9682076eb0c6089cb11
Matthias Ruester <ruester@molgen.mpg.de>
2012-03-04 21:05:26 +0100

    download.sh: download all files to /mnt/lfs/sources

--- download.sh
+++ download.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 LFS=/mnt/lfs
+DIR=${LFS}/sources
 
-mkdir -pv $LFS/usr/src
-chmod -v a+wt $LFS/usr/src
-wget -i wget-list -P $LFS/usr/src
-cp -v /usr/src/bee-git.tar $LFS/usr/src/bee-1.0.tar.gz
+mkdir -pv $DIR
+chmod a+wt $DIR
+wget --no-check-certificate -i wget-list -P $DIR -N -nv
+
+if [ -f $DIR/bee-1.0_rc23 ]; then
+    mv $DIR/bee-1.0_rc23 $DIR/bee-1.0_rc23.tar.gz
+fi
+
+if [ $(ls -1 $DIR | wc -l) -ne $(cat wget-list | wc -l) ]; then
+    echo "ERROR: some files could not be downloaded"
+else
+    echo "All downloads finished"
+fi
