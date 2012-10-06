40b0bb2368e69af2f8657459f8e15c13f88f7655
Matthias Ruester <ruester@molgen.mpg.de>
2012-10-06 11:19:36 +0200

    basic system: update the building script and make it more generic

--- basic_system/build_basic_system.sh
+++ basic_system/build_basic_system.sh
@@ -1,78 +1,76 @@
 #!/bin/bash -e
 
-ARCH=$(uname -m)
-
-for i in fhs-1.0 \
-         bee-1.0_rc23; do
-    if [ "$(/tools/bin/bee list -i ${i}-0.${ARCH} | wc -l)" = "0" ]; then
-        ./${i}-0.bee --no-archive-build --cleanup
-        /tools/bin/bee install ${i}-0.${ARCH}
+for i in fhs-*-0 \
+         bee-*-0; do
+    if [ "$(/tools/bin/bee list -i ${i%%-\**} | wc -l)" = "0" ]; then
+        /tools/bin/beesh ${i}.bee --no-archive-build --cleanup --install
     fi
 done
 
-for i in fhs-1.0 \
-    bee-1.0_rc23 \
-    linux-headers-3.2.6 \
-    man-pages-3.35 \
-    glibc-2.14.1 \
-    zlib-1.2.6 \
-    file-5.10 \
-    binutils-2.22 \
-    gmp-5.0.4 \
-    mpfr-3.1.0 \
-    mpc-0.9 \
-    gcc-4.6.2 \
-    sed-4.2.1 \
-    bzip2-1.0.6 \
-    ncurses-5.9 \
-    util-linux-2.20.1 \
-    psmisc-22.15 \
-    e2fsprogs-1.42 \
-    coreutils-8.15 \
-    iana-etc-2.30 \
-    m4-1.4.16 \
-    bison-2.5 \
-    procps-3.2.8 \
-    grep-2.10 \
-    readline-6.2 \
-    bash-4.2 \
-    libtool-2.4.2 \
-    gdbm-1.10 \
-    inetutils-1.9.1 \
-    perl-5.14.2 \
-    autoconf-2.68 \
-    automake-1.11.3 \
-    diffutils-3.2 \
-    gawk-4.0.0 \
-    findutils-4.4.2 \
-    flex-2.5.35 \
-    gettext-0.18.1.1 \
-    groff-1.21 \
-    xz-5.0.3 \
-    grub-1.99 \
-    gzip-1.4 \
-    iproute2-3.2.0 \
-    kbd-1.15.2 \
-    kmod-5 \
-    less-444 \
-    libpipeline-1.2.0 \
-    make-3.82 \
-    man-db-2.6.1 \
-    patch-2.6.1 \
-    shadow-4.1.5 \
-    sysklogd-1.5 \
-    sysvinit-2.88dsf \
-    tar-1.26 \
-    texinfo-4.13a \
-    udev-181 \
-    vim-7.3 \
-    nano-2.2.6; do
+for i in fhs-*-0 \
+    bee-*-0 \
+    linux-headers-*-0 \
+    man-pages-*-0 \
+    glibc-*-0 \
+    tzdata-*-0 \
+    zlib-*-0 \
+    file-*-0 \
+    binutils-*-0 \
+    gmp-*-0 \
+    mpfr-*-0 \
+    mpc-*-0 \
+    gcc-*-0 \
+    sed-*-0 \
+    bzip2-*-0 \
+    pkg-config-*-0 \
+    ncurses-*-0 \
+    util-linux-*-0 \
+    psmisc-*-0 \
+    e2fsprogs-*-0 \
+    shadow-*-0 \
+    coreutils-*-0 \
+    iana-etc-*-0 \
+    m4-*-0 \
+    bison-*-0 \
+    procps-*-0 \
+    grep-*-0 \
+    readline-*-0 \
+    bash-*-0 \
+    libtool-*-0 \
+    gdbm-*-0 \
+    inetutils-*-0 \
+    perl-*-0 \
+    autoconf-*-0 \
+    automake-*-0 \
+    diffutils-*-0 \
+    gawk-*-0 \
+    findutils-*-0 \
+    flex-*-0 \
+    gettext-*-0 \
+    groff-*-0 \
+    xz-*-0 \
+    grub-*-0 \
+    less-*-0 \
+    gzip-*-0 \
+    iproute2-*-0 \
+    kbd-*-0 \
+    kmod-*-0 \
+    libpipeline-*-0 \
+    make-*-0 \
+    man-db-*-0 \
+    patch-*-0 \
+    sysklogd-*-0 \
+    sysvinit-*-0 \
+    tar-*-0 \
+    texinfo-*-0 \
+    udev-lfs-*-0 \
+    vim-*-0 \
+    nano-*-0; do
 
-    if [ "$(/usr/bin/bee list -i ${i}-0.${ARCH} | wc -l)" = "0" ]; then
-        ./${i}-0.bee --no-archive-build --cleanup
-        /usr/bin/bee install ${i}-0.${ARCH}
+    if [ "$(/usr/bin/bee list -i ${i%%-\**} | wc -l)" = "0" ]; then
+        /usr/bin/beesh ${i}.bee --no-archive-build --cleanup --install
 
-        if [ "${i}" = "glibc-2.14.1" ]; then
+        if [ "${i}" = "tzdata-*-0" ]; then
             ./configuring_glibc.sh
             ./readjusting_toolchain.sh
         fi
