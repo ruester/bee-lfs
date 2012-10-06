0bd78f21d6a4e387cc9a06826329cf375474fdc3
Matthias Ruester <ruester@molgen.mpg.de>
2012-10-06 10:49:43 +0200

    basic system: update vkfs preparation script

--- basic_system/preparing_vkfs.sh
+++ basic_system/preparing_vkfs.sh
@@ -10,6 +10,12 @@ mknod -m 666 $LFS/dev/null c 1 3
 mount -v --bind /dev $LFS/dev
 
 mount -vt devpts devpts $LFS/dev/pts
-mount -vt tmpfs shm $LFS/dev/shm
 mount -vt proc proc $LFS/proc
 mount -vt sysfs sysfs $LFS/sys
+
+if [ -h /dev/shm ]; then
+   rm -f $LFS/dev/shm
+   mkdir $LFS/dev/shm
+fi
+
+mount -vt tmpfs shm $LFS/dev/shm
