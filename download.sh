a809dae3701472924fce5ab2a356bc07ea170a7f
Matthias Ruester <ruester@molgen.mpg.de>
2013-03-17 12:44:52 +0100

    Cleanup download script

--- download.sh
+++ download.sh
@@ -7,16 +7,8 @@ mkdir -pv $DIR
 chmod a+wt $DIR
 wget --no-check-certificate -i wget-list -P $DIR -N -nv
 
-if [ -f $DIR/bee-1.2 ]; then
-    mv $DIR/bee-1.2 $DIR/bee-1.2.tar.gz
-fi
-
 for i in $(cat wget-list); do
     A=$(basename $i)
-
-    if [ "${A}" = "bee-1.2" ]; then
-        A="${A}.tar.gz"
-    fi
 
     if [ ! -f ${DIR}/${A} ]; then
         echo "*** ERROR *** ${A} could not be downloaded"
